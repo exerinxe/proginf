c90cb3d3d1f4133d326f1f68332e9167095ac5bf
exerinxe <exerinxe@gmail.com>
2023-11-09 15:04:01 +0100

    jajo

---
+++ struktura programu.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+ using  namespace std;
+ int main(){ 
+
+ cout << "Wyswietlany tekst";
+ cout << endl;
+ cout << "Znak konca linii: \n";
+ cout << "Znak tabulatora: |t";
+ int a;
+ cin >> a;
+ cout << "Wartosc: " << a << " " << endl;
+ cout << a;
+ int b;
+ cin >> b;
+ cout << "Wartosc: " << b << " " << endl;
+ cout << b;
+int wynik1;
+ wynik1 = a / b;
+ 
+}
