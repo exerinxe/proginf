84cc9f1cfce37cd17f857fed3fb210ca2060aa12
exerinxe <exerinxe@gmail.com>
2023-11-22 14:06:24 +0100

    jajo

---
+++ petla while.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	// while(warunek-zakonczenia(true))
+	short a = 2;
+	while(a <= 30){  //czy wykonac to...?
+		cout << a << ",";
+		a += 2;
+	}
+	cout << endl << endl;
+	// do... while()
+	
+	short liczba;
+	do{
+		cout << "podaj liczbe: ";
+		cin >> liczba;
+	}while (liczba != 5 );  //czy powtorzyc to...?
+}
