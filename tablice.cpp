f2151b8d428d5b514eecd5442c5a8e7c3d4fbb05
exerinxe <exerinxe@gmail.com>
2023-12-06 14:09:34 +0100

    zqadania

---
+++ tablice.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int tablica [ 5 ];
+	
+	
+	int liczba[ 5 ];
+	for(int i = 0; i <5; i++)
+	
+	{
+		cin >> liczba[ i ];
+	}
+	
+	cout << "podales nastepujace liczby: ";
+	for(int i=0; i <5; i++)
+{
+	cout << liczba[ i ] << ", ";
+}
+	
+}
